cd358693d6de71e07d7e31b30d9c41358de67b35
Marcin Kwiatek <kwiatekm4@gmail.com>
2020-12-24 13:58:36 +0100

    add hide-show of textarea

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", onDOMLoaded)
+
+
+function onDOMLoaded(event)
+{
+    var addTaskButton=document.getElementById("add-task-button");
+    addTaskButton.onclick = function (event) {
+        addTaskButton.style.display = "none";
+        var addTaskTextarea=document.getElementById("add-task-textarea");
+        addTaskTextarea.style.display = "block";
+    };
+
+}
+
