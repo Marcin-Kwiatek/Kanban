82888c8aba4d98876932e1eb1e483ff3d70a02c6
Marcin Kwiatek <kwiatekm4@gmail.com>
2020-12-25 15:16:43 +0100

    refactor adding tasks

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 document.addEventListener("DOMContentLoaded", onDOMLoaded)
 
-
 function onDOMLoaded(event) {
-    var addTaskButton=document.getElementById("add-task-button");
-    addTaskButton.onclick = function (event) {
-        addTaskButton.style.display = "none";
-        var addTaskTextarea=document.getElementById("add-task-textarea");
-        addTaskTextarea.style.display = "block";
-        addTaskTextarea.focus()
-
-        addTaskTextarea.onkeydown = function(keyDownEvent) {
-            var keyCode = keyDownEvent.code;
-            if (keyCode == "Escape") {
-                addTaskTextarea.style.display = "none";
-                addTaskButton.style.display = "block";
-            }
-            if (keyCode == "Enter"){
-                keyDownEvent.preventDefault()
-                var nextTaskTitle = addTaskTextarea.value
-                if (nextTaskTitle==""){
-                   return 
-                }
-                
-                var newDiv = document.createElement("div")
-                newDiv.innerHTML = nextTaskTitle
-                newDiv.classList.add("task")
-                var parentNode = document.getElementById("list")
-                parentNode.insertBefore(newDiv,addTaskTextarea)
-                addTaskTextarea.value=null
-            }
+    var addTaskTextarea=document.getElementById("add-task-textarea")
+    var addTaskButton=document.getElementById("add-task-button")
+    
+    addTaskTextarea.onkeydown = function(keyDownEvent) {
+        var keyCode = keyDownEvent.code
+        if (keyCode == "Escape") {
+            addTaskTextarea.style.display = "none"
+            addTaskButton.style.display = "block"
         }
-        
-    };
+        if (keyCode == "Enter"){
+            keyDownEvent.preventDefault()
+            var nextTaskTitle = addTaskTextarea.value
+            if (nextTaskTitle==""){
+               return 
+            }
+            
+            var newDiv = document.createElement("div")
+            newDiv.innerHTML = nextTaskTitle
+            newDiv.classList.add("task")
+            var parentNode = document.getElementById("list")
+            parentNode.insertBefore(newDiv,addTaskTextarea)
+            addTaskTextarea.value=null
+        }
+    }
+
+    addTaskButton.onclick = function (event) {
+        addTaskButton.style.display = "none"
+        addTaskTextarea.style.display = "block"
+        addTaskTextarea.focus()
+    }
 
 }
 
