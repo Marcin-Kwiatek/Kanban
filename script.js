6e479a0a570e639f7ae616113e1aaee022afeb2f
Marcin Kwiatek <kwiatekm4@gmail.com>
2020-12-25 15:45:09 +0100

    add twice list and change var to let

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
 document.addEventListener("DOMContentLoaded", onDOMLoaded)
 
 function onDOMLoaded(event) {
-    var addTaskTextarea=document.getElementById("add-task-textarea")
-    var addTaskButton=document.getElementById("add-task-button")
-    
-    addTaskTextarea.onkeydown = function(keyDownEvent) {
-        var keyCode = keyDownEvent.code
-        if (keyCode == "Escape") {
-            addTaskTextarea.style.display = "none"
-            addTaskButton.style.display = "block"
-        }
-        if (keyCode == "Enter"){
-            keyDownEvent.preventDefault()
-            var nextTaskTitle = addTaskTextarea.value
-            if (nextTaskTitle==""){
-               return 
+    for (let i = 0; i <= 1; i++) {       
+        let addTaskTextarea=document.getElementById("add-task-textarea"+i)
+        let addTaskButton=document.getElementById("add-task-button"+i)
+        
+        addTaskTextarea.onkeydown = function(keyDownEvent) {
+            let keyCode = keyDownEvent.code
+            if (keyCode == "Escape") {
+                addTaskTextarea.style.display = "none"
+                addTaskButton.style.display = "block"
             }
-            
-            var newDiv = document.createElement("div")
-            newDiv.innerHTML = nextTaskTitle
-            newDiv.classList.add("task")
-            var parentNode = document.getElementById("list")
-            parentNode.insertBefore(newDiv,addTaskTextarea)
-            addTaskTextarea.value=null
+            if (keyCode == "Enter"){
+                keyDownEvent.preventDefault()
+                let nextTaskTitle = addTaskTextarea.value
+                if (nextTaskTitle==""){
+                    return 
+                }
+                
+                let newDiv = document.createElement("div")
+                newDiv.innerHTML = nextTaskTitle
+                newDiv.classList.add("task")
+                let parentNode = document.getElementById("list"+i)
+                parentNode.insertBefore(newDiv,addTaskTextarea)
+                addTaskTextarea.value=null
+            }
+        }
+        
+        addTaskButton.onclick = function (event) {
+            addTaskButton.style.display = "none"
+            addTaskTextarea.style.display = "block"
+            addTaskTextarea.focus()
         }
     }
-
-    addTaskButton.onclick = function (event) {
-        addTaskButton.style.display = "none"
-        addTaskTextarea.style.display = "block"
-        addTaskTextarea.focus()
-    }
-
 }
 
